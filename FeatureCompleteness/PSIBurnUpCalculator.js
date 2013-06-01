f344ad187b001eff14107eaf007fb04bb40d5aeb
Cédric Lignier <cedric.lignier@yahoo-inc.com>
2013-06-01 09:13:42 -0600

    Initial version

---
+++ FeatureCompleteness/PSIBurnUpCalculator.js
@@ -0,0 +1,24 @@
+Ext.define('Yahoo.app.PSIBurnUpCalculator', {
+	
+	extend: "Rally.data.lookback.calculator.TimeSeriesCalculator",
+
+	getMetrics: function () {
+        return [
+            {
+                "field": "PlanEstimate",
+                "as": "Planned",
+                "display": "line",
+                "f": "sum"
+            },
+            {
+                "field": "PlanEstimate",
+                "as": "Completed",
+                "f": "filteredSum",
+                "filterField": "ScheduleState",
+                "filterValues": ["Accepted", "Released"],
+                "display": "column"
+            }
+        ];
+    }
+	
+});
